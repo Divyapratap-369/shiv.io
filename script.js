3280aa18d57588f4563deeb0517eccbf4385fbf9
Divya pratap Singh <64465398+Divyapratap-369@users.noreply.github.com>
2023-01-26 23:11:43 +0530

    added html, css ,js

---
+++ script.js
@@ -0,0 +1,29 @@
+//Hide All section except the first one
+document.querySelectorAll("section").forEach(function(section){
+  if(section!==document.querySelector("section")){
+    section.style.display="none";
+  }
+});
+
+//Add Event Listener to the navigation links
+document.querySelectorAll("header nav a").forEach(function(link){
+  link.addEventListener("click",function(event){
+    event.preventDefault();
+    var sectionId = this.getAttribute("href").substr(1);
+    document.querySelectorAll("section").forEach(function(section){
+      if(section.getAttribute("id")===sectionId) {
+        section.style.display="block";
+      }
+      else{
+        section.style.display="none";
+      }
+    });
+  });
+});
+//Add Event Listeners To the Project links
+document.querySelectorAll("section#projects a").forEach(function(link){
+  link.addEventListener("click",function(event){
+    event.preventDefault();
+    alert("This feature has not been implemented yet");
+  });
+});
