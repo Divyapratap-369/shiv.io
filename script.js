175fb52f6ef3a4c23511d6606c4b71fb536c5af8
Divya pratap Singh <64465398+Divyapratap-369@users.noreply.github.com>
2023-02-07 20:49:23 +0530

    Added theme change functionality

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 //Hide All section except the first one
 document.querySelectorAll("section").forEach(function(section){
   if(section!==document.querySelector("section")){
@@ -20,13 +21,7 @@ document.querySelectorAll("header nav a").forEach(function(link){
     });
   });
 });
-// //Add Event Listeners To the Project links
-// document.querySelectorAll("section#projects a").forEach(function(link){
-//   link.addEventListener("click",function(event){
-//     event.preventDefault();
-//     alert("This feature has not been implemented yet");
-//   });
-// });
+
 setTimeout(() => {
   document.querySelector('.blur-overlay').style.opacity = '0';
 }, 1000);
@@ -35,3 +30,45 @@ const blurOverlay = document.querySelector('.blur-overlay');
 blurOverlay.addEventListener("transitionend", function(){
   blurOverlay.style.display = "none";
 });
+
+
+const toggle = document.getElementById('theme-toggle');
+const elements = document.querySelectorAll('header .nav-bar, .fa-brands');
+const image = document.getElementById('d-img');
+
+toggle.addEventListener('click', function() {
+  document.body.classList.toggle('light');
+  toggle.classList.toggle('spread');
+  toggle.classList.toggle('light');
+
+  if (document.body.classList.contains('light')) {
+    image.src='images/coloured-img.jpg'
+    document.body.style.backgroundColor = '#fff';
+    document.body.style.color = 'black';
+    elements.forEach(element => {
+      // element.style.backgroundColor = '#333';
+      element.addEventListener('mouseenter', function() {
+        this.style.color = '#EB455F';
+      });
+      element.addEventListener('mouseleave', function() {
+        this.style.color = 'black';
+      });
+      element.style.color = 'black';
+    });
+  } else {
+    image.src='images/dark-img.jpg';
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = '#fff';
+    elements.forEach(element => {
+      // element.style.backgroundColor = '#fff';
+      element.addEventListener('mouseenter', function() {
+        this.style.color = '#EB455F';
+      });
+      element.addEventListener('mouseleave', function() {
+        this.style.color = '#fff';
+      });
+      element.style.color = '#fff';
+    });
+  }
+});
+
